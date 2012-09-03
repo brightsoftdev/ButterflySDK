9f20aac862768fd5eb91226d8c779ea3ac9f38eb
Denny Kwon <info@tsunami234.com>
2012-09-02 21:59:49 -0400

    removed commented out code

--- butterflysdk/Favorites/HistoryViewController.m
+++ butterflysdk/Favorites/HistoryViewController.m
@@ -265,7 +265,6 @@
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
